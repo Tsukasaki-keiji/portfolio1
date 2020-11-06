1494cf3cc3f454ce9b9a06869d6359dbb13bf28d
EC2 Default User <ec2-user@ip-172-31-66-108.ec2.internal>
2020-11-06 08:11:01 +0000

    admin側機能

---
+++ app/controllers/admins/genres_controller.rb
@@ -0,0 +1,38 @@
+class Admins::GenresController < ApplicationController
+  #before_action :authenticate_admin!
+  
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+       flash[:notice] = "ジャンルを追加しました"
+       redirect_to admins_genres_path
+    else
+       @genres = Genre.all
+       render :index and return
+    end
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:success] = "ジャンルを変更しました"
+      redirect_to admins_genres_path
+    else
+      render :edit and return
+    end
+  end
+  
+  private
+  def genre_params
+    params.require(:genre).permit(:name, :is_active)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+
+  resources :genres, only: [:index, :create, :show, :edit, :update]
+  
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admins, controllers:{
+    sessions:'admins/sessions',
+    registrations:'admins/registrations',
+    passwords:'admins/passwords'
+  }
+  
+  namespace :admins do
+    get '/' => 'homes#top'
+    resources :genres, only: [:index, :create, :edit, :update]
+  end  
 end
