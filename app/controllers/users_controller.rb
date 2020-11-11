c0601b70e1d6de0ceaf094e0312a18678a38a3c5
EC2 Default User <ec2-user@ip-172-31-66-108.ec2.internal>
2020-11-11 02:27:00 +0000

    ユーザー編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,16 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @blogs = @user.blogs
+    @blogs = Blog.all
     @blog = Blog.new
   end
   
   def edit
     @user = User.find(params[:id])
+    @blogs = Blog.all
+    if @user != User.find(current_user.id)
+      redirect_to user_path(current_user)
+    end  
   end 
   
   def top
@@ -20,6 +24,7 @@ class UsersController < ApplicationController
   
   
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "You have updated user successfully."
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_one :user_profile, dependent: :destroy
+  has_many :blogs, dependent: :destroy
+  attachment :profile_image
+  
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  validates :name, length: { in: 2..20}
+  validates :introduction, length: { maximum:50 }
 end
